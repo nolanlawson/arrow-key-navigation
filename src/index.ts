188997a5914c0792dc2f03f946b27d3f34067340
Nolan Lawson <nolan@nolanlawson.com>
2020-06-19 14:32:20 -0700

    feat: shadow dom support using tree walker (#14)

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,6 @@
 
 interface FocusTrapTest { (element: Element): boolean }
 
-// This query is adapted from a11y-dialog
-// https://github.com/edenspiekermann/a11y-dialog/blob/cf4ed81/a11y-dialog.js#L6-L18
-var focusablesQuery = 'a[href], area[href], input, select, textarea, ' +
-  'button, iframe, object, embed, [contenteditable], [tabindex], ' +
-  'video[controls], audio[controls], summary'
-
 // TODO: email/number types are a special type, in that they return selectionStart/selectionEnd as null
 // As far as I can tell, there is no way to actually get the caret position from these inputs. So we
 // don't do the proper caret handling for those inputs, unfortunately.
@@ -22,27 +16,26 @@ var checkboxRadioInputTypes = ['checkbox', 'radio']
 
 var focusTrapTest: FocusTrapTest = undefined
 
-function getFocusableElements (activeElement) {
-  // Respect focus trap inside of dialogs
-  var dialogParent = getFocusTrapParent(activeElement)
-  var root = dialogParent || document
+// This query is adapted from a11y-dialog
+// https://github.com/edenspiekermann/a11y-dialog/blob/cf4ed81/a11y-dialog.js#L6-L18
+var focusablesQuery = 'a[href], area[href], input, select, textarea, ' +
+    'button, iframe, object, embed, [contenteditable], [tabindex], ' +
+    'video[controls], audio[controls], summary'
 
-  var res = []
-  var elements = root.querySelectorAll(focusablesQuery)
-
-  var len = elements.length
-  for (var i = 0; i < len; i++) {
-    var element = elements[i]
-    if (element === activeElement || (
-        !element.disabled &&
-        !/^-/.test(element.getAttribute('tabindex') || '') &&
-        !element.hasAttribute('inert') && // see https://github.com/GoogleChrome/inert-polyfill
-        (element.offsetWidth > 0 || element.offsetHeight > 0)
-    )) {
-      res.push(element)
-    }
+function getActiveElement () {
+  var activeElement = document.activeElement
+  while (activeElement.shadowRoot) {
+    activeElement = activeElement.shadowRoot.activeElement
   }
-  return res
+  return activeElement
+}
+
+function isFocusable (element) {
+  return element.matches(focusablesQuery) &&
+    !element.disabled &&
+    !/^-/.test(element.getAttribute('tabindex') || '') &&
+    !element.hasAttribute('inert') && // see https://github.com/GoogleChrome/inert-polyfill
+    (element.offsetWidth > 0 || element.offsetHeight > 0)
 }
 
 function getFocusTrapParent (element) {
@@ -58,7 +51,7 @@ function getFocusTrapParent (element) {
   }
 }
 
-function shouldIgnoreEvent (activeElement, key) {
+function shouldIgnoreEvent (activeElement, forwardDirection) {
   var tagName = activeElement.tagName
   var isTextarea = tagName === 'TEXTAREA'
   var isTextInput = tagName === 'INPUT' &&
@@ -85,36 +78,96 @@ function shouldIgnoreEvent (activeElement, key) {
 
   // if the cursor is inside of a textarea/input, then don't focus to the next/previous element
   // unless the cursor is at the beginning or the end
-  if (key === 'ArrowLeft' && selectionStart === selectionEnd && selectionStart === 0) {
+  if (!forwardDirection && selectionStart === selectionEnd && selectionStart === 0) {
     return false
-  } else if (key === 'ArrowRight' && selectionStart === selectionEnd && selectionStart === len) {
+  } else if (forwardDirection && selectionStart === selectionEnd && selectionStart === len) {
     return false
   }
   return true
 }
 
+function getNextCandidateNodeForShadowDomPolyfill (root, targetElement, forwardDirection, filter): HTMLElement {
+  // When the shadydom polyfill is running, we can't use TreeWalker on ShadowRoots because
+  // they aren't real Nodes. So we do this workaround where we run TreeWalker on the
+  // children instead.
+  var nodes = Array.prototype.slice.call(root.querySelectorAll('*'))
+  var idx = nodes.indexOf(targetElement)
+  if (forwardDirection) {
+    nodes = nodes.slice(idx + 1)
+  } else {
+    if (idx === -1) {
+      idx = nodes.length
+    }
+    nodes = nodes.slice(0, idx)
+    nodes.reverse()
+  }
+  for (var i = 0; i < nodes.length; i++) {
+    var node = nodes[i]
+    if (node instanceof HTMLElement && filter.acceptNode(node) === NodeFilter.FILTER_ACCEPT) {
+      return node
+    }
+  }
+  return undefined
+}
+
+function getNextCandidateNode (root, targetElement, forwardDirection, filter): HTMLElement {
+  var walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, filter)
+  if (targetElement) {
+    walker.currentNode = targetElement
+  }
+  if (forwardDirection) {
+    return walker.nextNode() as HTMLElement
+  } else if (targetElement) {
+    return walker.previousNode() as HTMLElement
+  }
+  // iterating backwards through shadow root, use last child
+  return walker.lastChild() as HTMLElement
+}
+
+function isShadowDomPolyfill () {
+  return typeof ShadowRoot !== 'undefined' &&
+      !ShadowRoot.toString().includes('[native code]')
+}
+
+function getNextNode (root, targetElement, forwardDirection): HTMLElement {
+  var filter: NodeFilter = {
+    acceptNode: function (node: HTMLElement) {
+      return (node === targetElement || node.shadowRoot || isFocusable(node))
+          ? NodeFilter.FILTER_ACCEPT
+          : NodeFilter.FILTER_SKIP
+    }
+  }
+
+  // TODO: remove this when we don't need to support the Shadow DOM polyfill
+  var nextNode = isShadowDomPolyfill() && root instanceof ShadowRoot
+    ? getNextCandidateNodeForShadowDomPolyfill(root, targetElement, forwardDirection, filter)
+    : getNextCandidateNode(root, targetElement, forwardDirection, filter)
+
+  if (nextNode && nextNode.shadowRoot) { // push into the shadow DOM
+    return getNextNode(nextNode.shadowRoot, null, forwardDirection)
+  }
+  if (!nextNode && (root as ShadowRoot).host) { // pop out of the shadow DOM
+    return getNextNode((root as ShadowRoot).host.getRootNode(), (root as ShadowRoot).host, forwardDirection)
+  }
+  return nextNode
+}
+
 function focusNextOrPrevious (event, key) {
-  var activeElement = document.activeElement
-  if (shouldIgnoreEvent(activeElement, key)) {
+  var activeElement = getActiveElement()
+  var forwardDirection = key === 'ArrowRight'
+  if (shouldIgnoreEvent(activeElement, forwardDirection)) {
     return
   }
-  var focusableElements = getFocusableElements(activeElement)
-  if (!focusableElements.length) {
-    return
+  var root = getFocusTrapParent(activeElement) || activeElement.getRootNode()
+  var nextNode = getNextNode(root, activeElement, forwardDirection)
+  if (nextNode && nextNode !== activeElement) {
+    nextNode.focus()
+    event.preventDefault()
   }
-  var index = focusableElements.indexOf(activeElement)
-  var element
-  if (key === 'ArrowLeft') {
-    element = focusableElements[index - 1] || focusableElements[0]
-  } else { // ArrowRight
-    element = focusableElements[index + 1] || focusableElements[focusableElements.length - 1]
-  }
-  element.focus()
-  event.preventDefault()
 }
 
 function handleEnter (event) {
-  var activeElement = document.activeElement
+  var activeElement = getActiveElement()
   if (activeElement.tagName === 'INPUT' &&
     checkboxRadioInputTypes.indexOf(activeElement.getAttribute('type').toLowerCase()) !== -1) {
     // Explicitly override "enter" on an input and make it fire the checkbox/radio
